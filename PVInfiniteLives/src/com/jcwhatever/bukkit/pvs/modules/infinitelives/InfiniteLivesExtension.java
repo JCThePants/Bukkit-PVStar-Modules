3129726d4843f82cf6e05e4c164b18b4daa980d3
jcThePants <jcthepants@gmail.com>
2014-11-04 15:51:16 -0800

    add PVInfiniteLives module

---
+++ PVInfiniteLives/src/com/jcwhatever/bukkit/pvs/modules/infinitelives/InfiniteLivesExtension.java
@@ -0,0 +1,60 @@
+/* This file is part of PV-Star Modules: PVInfiniteLives for Bukkit, licensed under the MIT License (MIT).
+ * 
+ * Copyright (c) JCThePants (www.jcwhatever.com)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+package com.jcwhatever.bukkit.pvs.modules.infinitelives;
+
+import com.jcwhatever.bukkit.generic.events.GenericsEventHandler;
+import com.jcwhatever.bukkit.generic.events.GenericsEventListener;
+import com.jcwhatever.bukkit.pvs.api.arena.extensions.ArenaExtension;
+import com.jcwhatever.bukkit.pvs.api.arena.extensions.ArenaExtensionInfo;
+import com.jcwhatever.bukkit.pvs.api.events.players.PlayerLivesChangeEvent;
+
+@ArenaExtensionInfo(
+        name = "PVInfiniteLives",
+        description = "Give players infinite lives in the arena.")
+public class InfiniteLivesExtension extends ArenaExtension implements GenericsEventListener {
+
+    @Override
+    protected void onEnable() {
+        getArena().getEventManager().register(this);
+    }
+
+    @Override
+    protected void onDisable() {
+        getArena().getEventManager().unregister(this);
+    }
+
+    @GenericsEventHandler
+    private void onLivesChange(PlayerLivesChangeEvent event) {
+
+        if (event.getPreviousLives() == 0)
+            return;
+
+        if (event.getNewLives() == 0) {
+            event.setNewLives(1);
+        } else {
+            event.setCancelled(true);
+        }
+    }
+}
